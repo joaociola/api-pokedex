08b8577fcda5f0352f508cb343846fe5c2e46aaf
joaociola <joaolucasciola1@gmail.com>
2023-09-03 21:32:50 -0300

    att

--- src/conexao.js
+++ src/conexao.js
@@ -5,7 +5,7 @@ const knex = require('knex')({
       host : process.env.DB_HOST,
       port : process.env.DB_PORT,
       user : process.env.DB_USER,
-      password :` "${process.env.DB_PASS}"`,
+      password :`"${process.env.DB_PASS}"`,
       database : process.env.DB_NAME,
       // ssl: { rejectUnauthorized: false },
     },
